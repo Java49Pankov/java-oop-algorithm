8adef6392213d443f124d2e7e559ce1d12ed603b
Evgeny Pankov <volvovnl123@gmail.com>
2023-05-03 21:12:40 +0300

    CW #9 MemoryService Exceptions

---
+++ src/telran/algorithm/test/MemoryServiceTest.java
@@ -0,0 +1,26 @@
+package telran.algorithm.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.algorithm.MemoryService;
+
+class MemoryServiceTest {
+	byte[] array;
+
+	@Test
+	void test() {
+		int size = MemoryService.getMaxAvailableSize();
+		array = new byte[size];
+		boolean flException = false;
+		try {
+			array = null;
+			array = new byte[size + 1];
+		} catch (OutOfMemoryError e) {
+			flException = true;
+		}
+		assertTrue(flException);
+	}
+
+}
